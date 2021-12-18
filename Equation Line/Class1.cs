c5700310a296cfe8123b7edffbf84ec1fce6cc34
Дмитрий Минаков <m1nak0vd@yandex.ru>
2021-12-18 22:18:34 +0300

    Добавьте файлы проекта.

---
+++ Equation Line/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Equation_Line
+{
+    public class Equation
+    {
+        public double k;
+        public double b;
+        public double x;
+
+        public void Root()
+        {
+            Console.WriteLine("Представлено линейное уравнение 0 = kx + b");
+            Console.Write("Введите число k = ");
+            k = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите число b = ");
+            b = Convert.ToDouble(Console.ReadLine());
+            if (b == 0) { Console.WriteLine("Ошибка!"); }
+            else { x = -(b / k); }
+        }
+    }
+}
---
+++ Task_11/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Equation_Line;
+
+namespace Task_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Equation Line = new Equation();
+            Line.Root();
+            Console.WriteLine("Число x = " + Line.x);
+            Console.ReadKey();
+        }
+    }
+}
